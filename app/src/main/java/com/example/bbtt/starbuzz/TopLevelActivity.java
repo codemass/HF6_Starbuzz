fc5fe3879dc787b55b3890d083e91adedd43e255
codemass <worklette@yandex.ru>
2018-07-20 02:03:18 +0300

    Добавлен слушатель событий ListView. При клике по первому пункту, вызывает активность DrinkCategoryActivity.

--- app/src/main/java/com/example/bbtt/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/example/bbtt/starbuzz/TopLevelActivity.java
@@ -1,7 +1,11 @@
 package com.example.bbtt.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        //Создать OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() { //itemClickListener - вложенный класс по отношению к классу AdapterView. ListView - субкласс AdapterView.
+            public void onItemClick (AdapterView<?> listView, //Представление на котром был сделан клик (списковое представление в данном случае).
+                                     View v, //Дополнительная информация о варианте спискового представления
+                                     int position, //Например представление и его позиция
+                                     long id) {
+                if (position == 0) { //Drinks - первый вариант в списковом представлении - находится в позиции 0.
+                    Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class); //Интент выдается TopLevelActivity. Должен запускать DrinkCategoryActivity.
+                    startActivity(intent);
+                }
+            }
+        };
+
+        //Добавить слушателя к списковому представлению
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener); //Добавление слушателя к ListView
     }
 }
