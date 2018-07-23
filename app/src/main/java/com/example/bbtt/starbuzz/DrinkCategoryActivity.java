24dae69189256413b7b24254e0a26a5cf933c0ba
codemass <worklette@yandex.ru>
2018-07-23 03:13:00 +0300

    Добавлен адаптер массива в DrinkCategoryActivity.

--- app/src/main/java/com/example/bbtt/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/bbtt/starbuzz/DrinkCategoryActivity.java
@@ -2,11 +2,19 @@ package com.example.bbtt.starbuzz;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DrinkCategoryActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ListView listDrinks = getListView();
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<Drink>( //ListView заполняется данными из массива drinks класса Drinks.
+          this,
+          android.R.layout.simlple_list_item_1,
+          Drink.drinks);
+        listDrinks.setAdapter(listAdapter);
     }
 }
