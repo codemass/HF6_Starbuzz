7ae57b173a4dd858de74ee0255d7cef7f5c00535
codemass <worklette@yandex.ru>
2018-07-24 21:04:33 +0300

    В DrinkCategoryActivity добавлен метод onListItemClick для вызова DrinkActivity.

--- app/src/main/java/com/example/bbtt/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/bbtt/starbuzz/DrinkCategoryActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
 
 public class DrinkCategoryActivity extends ListActivity {
 
@@ -16,5 +18,15 @@ public class DrinkCategoryActivity extends ListActivity {
           android.R.layout.simple_list_item_1,
           Drink.drinks);
         listDrinks.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onListItemClick (ListView listView,
+                                 View itemView,
+                                 int position,
+                                 long id) {
+        Intent intent = new Intent (DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra (DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity (intent);
     }
 }
